edcce3f6a6948c039884ecaff3ad696b6b63b5ff
RaySCS <rayaan.siddiqi@gmail.com>
2021-08-13 12:07:04 -0500

    Output file commited

---
+++ Output/Output/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Output
+//
+//  Created by Rayaan Siddiqi on 8/13/21.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    //cout is pronounced as "see-out". Object used with the insertion operator to print text.
+    return 0;
+}
