c73d800865ebe2128b4154efb897aa0c1097aa42
RaySCS <rayaan.siddiqi@gmail.com>
2021-08-13 12:25:06 -0500

    Added variable file

---
+++ Variables/Variables/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  Variables
+//
+//  Created by Rayaan Siddiqi on 7/13/21.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    
+    int num = 10; //Integer
+    char letter = 'a'; //Single letter
+    double myDouble = 3.65; //Floating point number. Note doubles can hold 15 decimal digits while floats can only hold 7 decimcal digits.
+    string myText = "Hello, World!"; //text
+    bool myBoolean = true; //true or false
+    
+    
+    /*
+     int - 4 bytes
+     float - 4 bytes
+     double - 8 bytes
+     boolean & char - 1 byte
+     */
+    return 0;
+}
