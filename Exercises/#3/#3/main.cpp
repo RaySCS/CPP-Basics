226d51c0258e7276e5d2637262abc7432bea791f
RaySCS <rayaan.siddiqi@gmail.com>
2021-08-16 16:59:32 -0500

    Added exercise #3

---
+++ Exercises/#3/#3/main.cpp
@@ -0,0 +1,35 @@
+/*
+ C++ Exercises
+ Author: Rayaan Siddiqi
+ */
+ 
+// OBJECTIVE: You will be given an array of N integers and you have to print the integers in the reverse order.
+
+
+#include <iostream>
+using namespace std;
+
+void performReverse();
+
+int main(int argc, const char * argv[]) {
+    performReverse();
+    
+    return 0;
+}
+
+void performReverse(){
+    int n;
+    cin >> n;
+    int numArry[n];
+    for (int x = 0; x<n;x++){
+        int currentNum;
+        cin >> currentNum;
+        numArry[x] = currentNum;
+    }
+    //original array has be constructed
+    
+    for (int y = n - 1; y>=0; y--){
+        // handles printing out reversed elements of arrays
+        cout << numArry[y] << " ";
+    }
+}
